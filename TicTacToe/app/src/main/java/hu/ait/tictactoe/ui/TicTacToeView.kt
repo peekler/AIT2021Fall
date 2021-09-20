0d08aad1a1a54a8c854606754226319d60b08fce
Péter Ekler <peter.ekler@aut.bme.hu>
2021-09-20 12:31:39 +0200

    TicTacToe circles list
    TicTacToe circles list

--- TicTacToe/app/src/main/java/hu/ait/tictactoe/ui/TicTacToeView.kt
+++ TicTacToe/app/src/main/java/hu/ait/tictactoe/ui/TicTacToeView.kt
@@ -4,13 +4,21 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.PointF
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
     lateinit var paintBackGround: Paint
     lateinit var paintLine: Paint
+
+    private var circles = mutableListOf<PointF>()
+
+    //var cX = -1f
+    //var cY = -1f
+
 
     init {
         paintBackGround = Paint()
@@ -27,8 +35,34 @@ class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, att
         super.onDraw(canvas)
 
         canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintBackGround)
-
         canvas?.drawLine(0f, 0f, width.toFloat(), height.toFloat(), paintLine)
+
+        //if (cX != -1f && cY != -1f) {
+        //    canvas?.drawCircle(cX, cY, 50f, paintLine)
+        //}
+
+        circles.forEach {
+            canvas?.drawCircle(it.x, it.y, 50f, paintLine)
+        }
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action == MotionEvent.ACTION_DOWN) {
+            //cX = event.x
+            //cY = event.y
+            circles.add(PointF(event.x, event.y))
+
+            invalidate() // redraw - calls the draw() method eventually
+        }
+
+        return true
+    }
+
+
+    public fun resetGame() {
+        circles.clear()
+        invalidate()
     }
 
 }
